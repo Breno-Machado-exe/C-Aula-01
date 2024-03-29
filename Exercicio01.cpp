88c7f747a07ddd9df1768c1d9cb6baaeab788b5c
Breno Machado <burracha.6464@gmail.com>
2024-03-29 15:23:41 -0300

    Adiciona Exercício 01

---
+++ Exercicio01.cpp
@@ -0,0 +1,15 @@
+/*Um aluno de Algoritmos fez um programa para imprimir o nome de alguns cursos do ICE. Mas ele cometeu alguns erros no código. Corrija o programa para que funcione corretamente.
+Result:
+Fisica
+Estatistica
+Matematica
+Quimica
+*/
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout << "Fisica\n" << "Estatistica\n" << "Matematica\n" << "Quimica" << endl;
+    return 0; 
+}
